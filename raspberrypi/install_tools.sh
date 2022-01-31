1c7bc40a9435e56c98ac2dd317b192f38492f6c4
brmazzoni <br.mazzoni@gmail.com>
2022-01-31 11:45:12 +0800

    added script to install missing tools in rpios

---
+++ raspberrypi/install_tools.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install git gh vim
